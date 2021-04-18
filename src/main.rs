dadca121dc5054cc633bde3e256f81494bf07f89
Horimatsu Takuya <new_woory@hotmail.com>
2021-04-18 09:56:43 +0900

    file check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::path::Path;
+use std::{thread, time};
+
+fn main() -> std::io::Result<()> {
+    let filename = env::args().nth(1).expect("file name required");
+
+    let exists = Path::new(&filename).exists();
+
+    let mut found = false;
+    for _i in  0..5{
+        if exists {
+            found = true;
+            break;
+        } else {
+            let wait = time::Duration::from_millis(1000);
+            println!("waiting");
+            thread::sleep(wait);
+        }
+    }
+    if found {
+        Ok(())
+    } else {
+        panic!("file didn't appear");
+    }
 }
